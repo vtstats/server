e50479ea8e3b8c14e383cc03dae807a9b928744a
PoiScript <poiscript@gmail.com>
2023-07-23 01:08:26 +0800

    chore: don't setup tracing and metrics when database migration

--- main.rs
+++ main.rs
@@ -1,15 +1,20 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    vtstat_utils::metrics::install();
-    let _guard = vtstat_utils::tracing::init();
-
     let command = std::env::args().nth(1).unwrap_or_default();
+
+    if command == "database-migrate" {
+        vtstat_database::migrate().await?;
+        println!("Database migration success");
+        return Ok(());
+    }
+
+    vtstat_utils::metrics::install();
+    vtstat_utils::tracing::init();
 
     match command.as_str() {
         "standalone" => tokio::try_join!(vtstat_web::main(), vtstat_worker::main()).map(|_| ()),
         "web" => vtstat_web::main().await,
         "worker" => vtstat_worker::main().await,
-        "database-migrate" => vtstat_database::migrate().await,
         c => anyhow::bail!("Unknown command {:?}", c),
     }
 }
