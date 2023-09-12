93fd45d3dc62b61477381d1b4cc0dd4ff1202867
PoiScript <poiscript@gmail.com>
2023-09-12 18:52:48 +0800

    fix(database): handle empty jobs table

--- vtstats-database/jobs/next_queued.rs
+++ vtstats-database/jobs/next_queued.rs
@@ -3,9 +3,9 @@ use sqlx::{PgPool, Result};
 
 pub async fn next_queued(pool: &PgPool) -> Result<Option<DateTime<Utc>>> {
     let query =
-        sqlx::query!("SELECT min(next_run) FROM jobs WHERE status = 'queued'").fetch_one(pool);
+        sqlx::query!("SELECT min(next_run) FROM jobs WHERE status = 'queued'").fetch_optional(pool);
 
     let record = crate::otel::execute_query!("SELECT", "jobs", query)?;
 
-    Ok(record.min)
+    Ok(record.and_then(|i| i.min))
 }
