79ac74b7b345bdcc6070963bc38d244d32d7dce5
PoiScript <poiscript@gmail.com>
2023-10-22 16:16:12 +0800

    fix(api): pubsub won't send signature when verifing intent

--- vtstats-api/pubsub/mod.rs
+++ vtstats-api/pubsub/mod.rs
@@ -1,22 +1,24 @@
 pub mod publish;
 pub mod verify;
 
-use axum::{middleware, routing::get, Router};
+use axum::{middleware, routing::post, Router};
 use integration_youtube::pubsub::verify;
-
-pub use publish::publish_content;
 use tower::ServiceBuilder;
 use tower_http::ServiceBuilderExt;
+
+pub use publish::publish_content;
 pub use verify::verify_intent;
 use vtstats_database::PgPool;
 
 pub fn router(pool: PgPool) -> Router {
+    let verify_layer = ServiceBuilder::new()
+        .map_request_body(axum::body::boxed)
+        .layer(middleware::from_fn(verify));
+
     Router::new()
-        .route("/", get(verify_intent).post(publish_content))
-        .layer(
-            ServiceBuilder::new()
-                .map_request_body(axum::body::boxed)
-                .layer(middleware::from_fn(verify)),
+        .route(
+            "/",
+            post(publish_content).layer(verify_layer).get(verify_intent),
         )
         .with_state(pool)
 }
